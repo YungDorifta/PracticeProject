0b5eab509e63bbdbe55a09258fae12d2918b3c85
arsbelashev <arsbelashev@gmail.com>
2023-05-18 15:24:12 +0300

    Full info window: view images from paths fetched from DB

--- App1/FullInfoWindow.xaml.cs
+++ App1/FullInfoWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Controls;
 using System.Data.SqlClient;
 using System.Data;
 using System.Configuration;
+using System.Windows.Media.Imaging;
 
 namespace PhotoViewer
 {
@@ -12,50 +13,45 @@ namespace PhotoViewer
     /// </summary>
     public partial class FullInfoWindow : Window
     {
-
+        //cтрока подключения к БД
         string connectionString;
-        SqlDataAdapter adapter;
-        DataTable OriginalsTable;
-        Label lab;
         
+        /// <summary>
+        /// Конструктор окна доп. информации
+        /// </summary>
         public FullInfoWindow()
         {
             InitializeComponent();
             connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            this.lab = Lab;
         }
 
+        /// <summary>
+        /// Загрузка окна доп. информации
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            this.lab.Content = "BRUH";
-
-            string sql = "SELECT * FROM dbo.Originals";
-            OriginalsTable = new DataTable();
+            //сброс соединения
             SqlConnection connection = null;
-
-
+            
             try
             {
+                //подключение к БД используя строку подключения
                 connection = new SqlConnection(connectionString);
-                SqlCommand command = new SqlCommand(sql, connection);
-                adapter = new SqlDataAdapter(command);
-
-                // установка команды на добавление для вызова хранимой процедуры
-                adapter.InsertCommand = new SqlCommand("sp_InsertPhone", connection);
-                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Date", SqlDbType.DateTime, 50, "Date"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Picturepath", SqlDbType.NChar, 50, "Picturepath"));
-                SqlParameter parameter = adapter.InsertCommand.Parameters.Add("@OriginalID", SqlDbType.Int, 1, "OriginalID");
-                parameter.Direction = ParameterDirection.Output;
-
+                
+                //открытие подключения
                 connection.Open();
-                adapter.Fill(OriginalsTable);
-                OriginalsGrid.ItemsSource = OriginalsTable.DefaultView;
 
-                if (connection != null)
-                {
-                    lab.Content = "BRUH";
-                }
+                //вывод картинок по путям из БД
+                string SQL = "SELECT Picturepath FROM dbo.Originals WHERE OriginalID=2";
+                LoadImage(originalImage, SQL, connection);
+                SQL = "SELECT Picturepath FROM dbo.Markups WHERE OriginalID=2";
+                LoadImage(markupImage, SQL, connection);
+
+                //заполнение таблицы информацией
+                SQL = "SELECT * FROM dbo.Originals";
+                LoadTable(FullInfoTable, SQL, connection);
             }
             catch (Exception ex)
             {
@@ -68,10 +64,84 @@ namespace PhotoViewer
             }
         }
 
+        /// <summary>
+        /// Вывод информации из таблицы БД в таблицу в окне
+        /// </summary>
+        /// <param name="table">Элемент таблицы в окне</param>
+        /// <param name="SQL">Запрос для извлечения данных</param>
+        /// <param name="connection">Подключение к БД</param>
+        private void LoadTable(DataGrid table, string SQL, SqlConnection connection)
+        {
+            try
+            {
+                //таблица для хранения информации, извлеченной из БД
+                DataTable keepTable = new DataTable();
+            
+                //полная команда (SQL query + connection query) для извлечения информации
+                SqlCommand command = new SqlCommand(SQL, connection);
+
+                //адаптер для извлечения информации из БД в Таблицу
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                // установка команды на добавление для вызова хранимой процедуры
+                adapter.InsertCommand = new SqlCommand("sp_InsertPhone", connection);
+                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Date", SqlDbType.DateTime, 50, "Date"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Picturepath", SqlDbType.NChar, 50, "Picturepath"));
+
+                //???
+                SqlParameter parameter = adapter.InsertCommand.Parameters.Add("@OriginalID", SqlDbType.Int, 1, "OriginalID");
+                parameter.Direction = ParameterDirection.Output;
+
+                //заполнение адаптером таблицы хранения информации из БД
+                adapter.Fill(keepTable);
+
+                //заполнение элемента таблицы информацией из таблицы хранения
+                table.ItemsSource = keepTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Вывод изображения по пути извлеченному из БД в окно
+        /// </summary>
+        /// <param name="image">Элемент изображения в окне</param>
+        /// <param name="SQL">Запрос по которому извлекается путь к изображению</param>
+        /// <param name="connection">Подключение к БД</param>
+        private void LoadImage(Image image, string SQL, SqlConnection connection)
+        {
+            try
+            {
+                //получение пути к изображению из БД
+                SqlCommand command = new SqlCommand(SQL, connection);
+                string imageUri = (string)command.ExecuteScalar();
+
+                //вывод изображения из БД в BMP
+                BitmapImage myBitmapImage = new BitmapImage();
+                myBitmapImage.BeginInit();
+                myBitmapImage.UriSource = new Uri(imageUri);
+                myBitmapImage.EndInit();
+
+                //вывод изображения из BMP в окно
+                image.Source = myBitmapImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Возвращение на главное окно
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MainWindow MW = new MainWindow();
-
             MW.Show();
             this.Close();
         }
